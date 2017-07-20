f5cca5beb436bb98c899a7295b9f4956b2460d75
chani hoffman <hoffmanchani@gmail.com>
2017-07-20 11:21:35 +0300

    move last version to cdn library

--- staging/englishon.js
+++ staging/englishon.js
@@ -4951,6 +4951,32 @@
   }(f);return m(w, { Tour: T, Step: O, Evented: f }), w;
 });
 //
+(function (e, a) {
+  if (!a.__SV) {
+    var b = window;try {
+      var c,
+          l,
+          i,
+          j = b.location,
+          g = j.hash;c = function (a, b) {
+        return (l = a.match(RegExp(b + "=([^&]*)"))) ? l[1] : null;
+      };g && c(g, "state") && (i = JSON.parse(decodeURIComponent(c(g, "state"))), "mpeditor" === i.action && (b.sessionStorage.setItem("_mpcehash", g), history.replaceState(i.desiredHash || "", e.title, j.pathname + j.search)));
+    } catch (m) {}var k, h;window.mixpanel = a;a._i = [];a.init = function (b, c, f) {
+      function e(b, a) {
+        var c = a.split(".");2 == c.length && (b = b[c[0]], a = c[1]);b[a] = function () {
+          b.push([a].concat(Array.prototype.slice.call(arguments, 0)));
+        };
+      }var d = a;"undefined" !== typeof f ? d = a[f] = [] : f = "mixpanel";d.people = d.people || [];d.toString = function (b) {
+        var a = "mixpanel";"mixpanel" !== f && (a += "." + f);b || (a += " (stub)");return a;
+      };d.people.toString = function () {
+        return d.toString(1) + ".people (stub)";
+      };k = "disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
+      for (h = 0; h < k.length; h++) e(d, k[h]);a._i.push([b, c, f]);
+    };a.__SV = 1.2;b = e.createElement("script");b.type = "text/javascript";b.async = !0;b.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ? MIXPANEL_CUSTOM_LIB_URL : "file:" === e.location.protocol && "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c = e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b, c);
+  }
+})(document, window.mixpanel || []);
+mixpanel.init("6077b9f24b24eccfecc454882f981304");
+//
 //var configStorage;
 // in the real world we will store config on the server
 // and do some CORS voodoo to fetch it.
@@ -8549,7 +8575,7 @@ var EnglishOnMenu = function () {
           document.tour.start();
           clearInterval(document.tutorialInterval);
         }
-      }, 3000);
+      }, 500);
       return false;
     }
   });
@@ -8576,7 +8602,6 @@ e$.when(document.questions_promise).done(function () {
 // })
 
 e$.when(document.resources_promise, document.loaded_promise).done(function () {
-  //addMixPannel();
   //event to get messageses from englishon backend
   window.addEventListener("message", receiveMessage, false);
   //register the handler for backspace/forward
@@ -8681,17 +8706,6 @@ function receiveMessage(event) {
     document.eoDialogs.hideDialogs(0);
   }
 }
-
-addMixPannel = function () {
-  //   <!-- start Mixpanel --><script type="text/javascript">(function(e,a){if(!a.SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,
-  // 0)))}}var d=a;"undefined"!==typeof f?d=a[f]=[]:f="mixpanel";d.people=d.people||[];d.toString=function(b){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);b||(a+=" (stub)");return a};d.people.toString=function(){return d.toString(1)+".people (stub)"};k="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
-  // for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);
-  // mixpanel.init("d717e9bb23923da98cd52a637644d933");</script><!-- end Mixpanel -->
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = document.englishonConfig.backendUrl + '/static/ex/mixpanel.js';
-  document.head.append(script);
-};
 //
 // *********
 // Name List
